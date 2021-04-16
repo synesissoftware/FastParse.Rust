29c7471b7d92629621a8b9963b551c79653510c7
Matt Wilson <matthew@synesis.com.au>
2021-04-16 18:44:49 +1000

    SliceIndex : + added move_unchecked() method

--- src/lib.rs
+++ src/lib.rs
@@ -101,6 +101,32 @@ pub mod fastparse {
 
                 0 == self.length
             }
+
+            /// Obtains unchecked a copy of the slice moved by the given
+            /// `d`elta
+            ///
+            /// Parameters:
+            /// * `d` - The delta
+            ///
+            /// Preconditions:
+            /// * `isize <= self.offset` - will panic (in debug) if false
+            pub fn move_unchecked(&self, d : isize) -> Self {
+
+                // TODO: determine the rigth Rust way of doing addition with
+                let new_off : usize = if d < 0 {
+
+                    self.offset - (-d) as usize
+                } else {
+
+                    self.offset + d as usize
+                };
+
+                Self {
+
+                    length  :   self.length,
+                    offset  :   new_off,
+                }
+            }
         }
     }
 }
@@ -186,5 +212,37 @@ fn SliceIndex_op_lt() {
 
     assert!(SliceIndex::new(0, 1) < SliceIndex::new(0, 2));
     assert!(SliceIndex::new(0, 2) > SliceIndex::new(0, 1));
+}
+
+#[test]
+#[allow(non_snake_case)]
+fn SliceIndex_move_unchecked() {
+
+    use fastparse::types::SliceIndex;
+
+    {
+        let ssi1 = SliceIndex::new(0, 1);
+
+        let ssi2 = ssi1.move_unchecked(1);
+
+        assert_eq!(SliceIndex::new(1, 1), ssi2);
+    }
+
+    {
+        let ssi1 = SliceIndex::new(1, 1);
+
+        let ssi2 = ssi1.move_unchecked(-1);
+
+        assert_eq!(SliceIndex::new(0, 1), ssi2);
+    }
+
+    #[cfg(not(debug_assertions))]
+    {
+        let ssi1 = SliceIndex::new(0, 1);
+
+        let ssi2 = ssi1.move_unchecked(-1);
+
+        assert_eq!(SliceIndex::new(std::usize::MAX, 1), ssi2);
+    }
 }
 
